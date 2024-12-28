7204f94e0524ed0ebacef263cdb1180f2e9eaa1f
jm90hong <jm90.hong@gmail.com>
2024-12-28 09:21:21 +0900

    init

---
+++ data-2.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+a = np.array([1, 2, 3, 4, 5])
+print(a[0])  # [1 2 3 4 5]
+
+b = np.array([[1, 2, 3], [4, 5, 6]])
+print(b[0][2])
+# [[1 2 3]
+#  [4 5 6]]
---
+++ data-3.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('employee_data.csv')
+
+# print(df.head())
+# print('==========[1]==========')
+# print(df.info())
+# print('==========[2]==========')
+# print(df.describe())
+
+
+
+# 이름 데이터 추출
+names = df['이름']
+#print(names.head())
+
+
+first_row = df.iloc[0]
+#print(first_row)
+
+older_than_30 = df[df['나이'] > 30]
+
+# print(df['나이'] > 30)
+# print(older_than_30.head())
+
+grouped_df = df.groupby('부서')['나이'].mean()
+#print(grouped_df)
+
+
+
+df['연령대'] = df['나이'].apply(lambda x: '30대' if 30 <= x < 40 else '30대 이하' if x < 30 else '40대 이상')
+
+df.to_csv('modified_employee_data.csv', index=False)
